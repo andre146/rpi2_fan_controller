1f9ba405c40b051331eea9be7670583987a8ac63
Andre Picker <andrepicker@live.de>
2017-12-17 13:24:40 +0100

    added loading of config file, not reading it yet

--- fan.c
+++ fan.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 
 #define TEMPFILE_PATH "/sys/class/thermal/thermal_zone0/temp"
 #define TEMP_STRING_LEN 5
@@ -11,9 +12,8 @@
 #define PWM_RANGE 1024
 #define SLEEP_TIME 3
 #define LOWEST_ERROR -3
-
-#define PROP_GAIN 1
-#define INT_GAIN 0.2
+#define CONFIGFILE_PATH "fan.conf"
+#define LINE_BUF_LEN 1024
 
 float getTemp(){
 
@@ -34,7 +34,7 @@ float getTemp(){
 
 void cleanup(){
 
-	pwmWrite(FAN_PIN, 0);
+	pwmWrite(1, 0);
 	printf("Exit...\n");
 	exit(0);
 }
@@ -48,27 +48,53 @@ int main(int argc, char **argv){
 
 	int setTemp = atoi(argv[1]);
 	int effort = 0;
+	int sleepTime = 5;
+	char fanPin = 1;
 	float error = 0;
 	float errorSum = 0;
 	float propEffort = 0;
 	float intEffort = 0;
+	float propGain = 1;
+	float intGain = 1;
 
 	signal(SIGINT, cleanup);
 	wiringPiSetup();
 	pwmSetRange(PWM_RANGE);
 	//pwmSetClock(3840);
 	pwmSetMode(PWM_MODE_MS);
-	pinMode(FAN_PIN, PWM_OUTPUT);
+	pinMode(fanPin, PWM_OUTPUT);
 
 	printf("Starting PI-Based fan controller v1.0 by Andre Picker\n");
+
+	FILE *confFile = fopen(CONFIGFILE_PATH, "rb");
+	long fileLen;
+	char *fileBuffer;
+	char *lineBuffer;
+	char *cmdBuffer;
+	if(confFile == NULL){
+		printf("Failed to open config file fan.conf!\n");
+		return(1);
+	}
+
+	fseek(confFile, 0, SEEK_END);
+	fileLen = ftell(confFile) + 1;
+	fseek(confFile, 0, SEEK_SET);
+	fileBuffer = malloc(fileLen + 1);
+	fread(fileBuffer, 1, fileLen, confFile);
+	fclose(confFile);
+	lineBuffer = malloc(LINE_BUF_LEN);
+	cmdBuffer = malloc(LINE_BUF_LEN);
+
+	lineBuffer = strtok(fileBuffer, "\n");
+
 
 	while(1){
 
 		error = getTemp() - setTemp;
 		errorSum += error;
 
-		propEffort = PROP_GAIN * error;
-		intEffort = INT_GAIN * errorSum * SLEEP_TIME;
+		propEffort = propGain * error;
+		intEffort = intGain * errorSum * sleepTime;
 		effort = (int)round(propEffort + intEffort);
 
 		if(effort > PWM_RANGE){
@@ -78,14 +104,14 @@ int main(int argc, char **argv){
 		}
 
 		if(intEffort > PWM_RANGE){
-			errorSum = PWM_RANGE / SLEEP_TIME / INT_GAIN;
+			errorSum = PWM_RANGE / sleepTime / intGain;
 		}
 		if(error <= LOWEST_ERROR){
 			errorSum = 0;
 		}
 
-		pwmWrite(FAN_PIN, effort);
-		sleep(SLEEP_TIME);
+		pwmWrite(fanPin, effort);
+		sleep(sleepTime);
 	}
 
 	return 0;
